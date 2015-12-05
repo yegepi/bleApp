62ccac8e5e6ac646b4bdbb6b9c41ac5d6a192db5
KS <ks@KSs-Mac-mini.local>
2015-12-05 11:24:57 +0800

    Initial Commit

---
+++ BLEThermometer/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  BLEThermometer
+//
+//  Created by KS on 5/12/15.
+//  Copyright (c) 2015年 KS. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ BLEThermometer/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BLEThermometer
+//
+//  Created by KS on 5/12/15.
+//  Copyright (c) 2015年 KS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
